a0fe7cef1eb4cca8fe89b970a74cbaff09ad6c1c
しまぶー <0120koto@gmail.com>
2024-01-29 11:35:24 +0900

    ✨

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,18 @@
-import type { MetaFunction } from "@remix-run/node";
+import * as stylex from "@stylexjs/stylex";
 
-export const meta: MetaFunction = () => {
-	return [
-		{ title: "New Remix App" },
-		{ name: "description", content: "Welcome to Remix!" },
-	];
-};
+const styles = stylex.create({
+	foo: {
+		fontSize: "3rem",
+	},
+	bar: {
+		backgroundColor: "yellow",
+	},
+});
 
 export default function Index() {
 	return (
-		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-			<h1>Welcome to Remix!</h1>
-			<ul>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/blog"
-						rel="noreferrer"
-					>
-						15m Quickstart Blog Tutorial
-					</a>
-				</li>
-				<li>
-					<a
-						target="_blank"
-						href="https://remix.run/tutorials/jokes"
-						rel="noreferrer"
-					>
-						Deep Dive Jokes App Tutorial
-					</a>
-				</li>
-				<li>
-					<a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-						Remix Docs
-					</a>
-				</li>
-			</ul>
+		<div>
+			<div {...stylex.props(styles.foo, styles.bar)}>Vite + Remix + StyleX</div>
 		</div>
 	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import { stylexPlugin } from "vite-plugin-stylex-dev";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [remix(), tsconfigPaths()],
+	plugins: [remix(), stylexPlugin(), tsconfigPaths()],
 });
